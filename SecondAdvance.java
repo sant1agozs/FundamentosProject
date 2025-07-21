5741f50b8abdb554ea0a95d48803899a5e0070d0
Santiago Hernandez <ksant.hernandez@gmail.com>
2025-07-21 01:01:25 -0600

    Update and rename App.java to SecondAdvance.java

--- SecondAdvance.java
+++ SecondAdvance.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 
-public class App {
+public class SecondAdvance {
   static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
   static PrintStream out = System.out;
 
